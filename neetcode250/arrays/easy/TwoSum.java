fdabc0ec189c1775305ee49ba92d7121f9efd135
Nagarjun <nagarjun20n@gmail.com>
2025-06-19 22:38:26 +0530

    two sum

--- neetcode250/arrays/easy/TwoSum.java
+++ neetcode250/arrays/easy/TwoSum.java
@@ -1,6 +1,7 @@
 package neetcode250.arrays.easy;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
 
@@ -15,13 +16,24 @@ public class TwoSum {
         return new int[0];
     }
 
-//    public static int [] twoSumBetter(int arr[],int target){
-//
-//    }
+    public static int [] twoSumBetter(int arr[],int target){
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            int compliment=target-arr[i];
+            if(map.containsKey(compliment)){
+                return new int []{map.get(compliment),i};
+            }
+            map.put(arr[i],i);
+        }
+        return new int[0];
+    }
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5};
         int[] twoSum = twoSumBrute(arr,7);
         System.out.println("two sum--->"+ Arrays.toString(twoSum));
+
+        int [] twoSumHash=twoSumBetter(arr,7);
+        System.out.println("hasing---->"+Arrays.toString(twoSumHash));
 
 
 
